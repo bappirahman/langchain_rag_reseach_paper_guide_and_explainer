926b23d26d8705fcb3aac7fa1716fde68489f564
Bappi Rahman <cs.bappirahman@gmail.com>
2024-06-16 11:45:36 +0600

    Updated title

--- src/app.py
+++ src/app.py
@@ -15,6 +15,7 @@ load_dotenv()
 
 os.environ['GROQ_API_KEY'] = os.getenv('GROQ_API_KEY')
 
+st.title('Research Paper Guide and Explainer')
 
 model_name = "sentence-transformers/all-mpnet-base-v2"
 model_kwargs = {'device': 'cpu'}
@@ -26,17 +27,14 @@ hf_emb = HuggingFaceEmbeddings(
 )
 
 
-def load_vector():
+def load_vector(query):
     """
-    Load the vector embeddings for the documents.
+    Load the data from Arxviv and store the vector embeddings.
 
-    This function checks if the 'vector' key is present in the session state. If not, it loads the documents from the
-    given queries using the ArxivLoader class. The documents are then split into chunks using the RecursiveCharacterTextSplitter
-    class with a chunk size of 1000 characters and a chunk overlap of 0. Finally, the vector embeddings are computed using
-    the FAISS class with the help of the hf_emb object.
+    This function checks if the 'vector' key is present in the session state. If not, it loads the documents from the given queries using the ArxivLoader class. The documents are then split into chunks using the RecursiveCharacterTextSplitter class with a chunk size of 1000 characters and a chunk overlap of 100. Finally, the vector embeddings are computed using the FAISS class with the help of the hf_emb object.
 
     Parameters:
-    None
+    - query (str): The query string used to load the documents.
 
     Returns:
     None
@@ -44,25 +42,56 @@ def load_vector():
     if 'vector' not in st.session_state:
       start = time.time()
       print(start)
-      queries = ['2302.13971', '2307.09288']
-      documents = []
       
-      for query in queries:
-          loader = ArxivLoader(query=query, load_max_docs=1)
-          documents.extend(loader.load())
-      st.session_state.documents = documents
-      st.session_state.docs = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0).split_documents(st.session_state.documents)
+      st.session_state.documents = ArxivLoader(query=query, load_max_docs=1).load()
+      st.session_state.docs = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100).split_documents(st.session_state.documents)
       print('Embedding started') 
       embedding_start = time.time()
-      st.session_state.vector = FAISS.from_documents(st.session_state.docs, hf_emb)
+      st.session_state.vector = FAISS.from_documents(st.session_state.docs[:10], hf_emb)
       print('Embedding done')
       print('Embedding done in', (time.time() - embedding_start) / 60, 'minutes')
-      st.write('It took {time.time() - start} to load the data')
+      
+      st.write(f'It took {(time.time() - start) / 60} minutes to load the data')
 
-chat = ChatGroq(
-    temperature=0,
-    model="llama3-70b-8192"
+def create_retriever(prompt_template):
+  """
+  Creates a retriever chain using the ChatGroq model and the provided prompt template.
+
+  Args:
+      prompt_template (ChatPromptTemplate): The prompt template to be used in the retriever chain.
+
+  Returns:
+      RetrievalChain: The retriever chain with the ChatGroq model and the provided prompt template.
+  """
+  chat = ChatGroq(
+    temperature=0.8,
+    model="llama3-70b-8192",
+    verbose=True
 ) 
+  chain = create_stuff_documents_chain(
+  prompt=prompt_template,
+  llm=chat
+  )
+  retriever = st.session_state.vector.as_retriever()
+  retriever_chain = create_retrieval_chain(
+    retriever=retriever,
+    combine_docs_chain=chain
+  )
+  return retriever_chain
+def ask(retriever_chain, input):  
+  """
+  Invokes the retriever chain with the given input and writes the answer to the Streamlit output.
+
+  Args:
+      retriever_chain (RetrievalChain): The retriever chain to invoke.
+      input (str): The input to the retriever chain.
+
+  Returns:
+      None
+  """
+  response = retriever_chain.invoke({"input": input})
+  st.write(response["answer"])
+
 
 prompt_template = ChatPromptTemplate.from_messages([
   """
@@ -75,24 +104,18 @@ prompt_template = ChatPromptTemplate.from_messages([
   </question>
   """
 ])
-if st.button('Load Data'):
-  st.write('Please wait while we load the data...')
-  load_vector()
-  st.write('Data loaded successfully!')
-input = st.text_input("Enter Your question about llama, llama2 from it's research papers")
 
-if input:
-  chain = create_stuff_documents_chain(
-    prompt=prompt_template,
-    llm=chat
-  )
-  retriever = st.session_state.vector.as_retriever()
-  retriever_chain = create_retrieval_chain(
-    retriever=retriever,
-    combine_docs_chain=chain
-  )
-  response = retriever_chain.invoke({"input": input})
-  st.write(response["answer"])
+query = st.text_input('Enter arxiv query number of the research paper (eg. 2312.00518)').strip()
+      
+if query:
+  st.write('Please wait while we load the data...')
+  load_vector(query)
+  retriever_chain = create_retriever(prompt_template)
+  st.write('Data loaded successfully!')
+  input = st.text_input("Enter Your question about your specific research papers")
+  ask(retriever_chain, input)
+
+
 
 
 
